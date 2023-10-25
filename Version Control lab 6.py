d5cd19c2c74f4a0173c51742d77676b02c70aaec
alexj <alexjvd@outlook.com>
2023-10-25 12:41:52 -0400

    full lab6

--- Version Control lab 6.py
+++ Version Control lab 6.py
@@ -1,5 +1,21 @@
+# Alejandro Velez's decode function
+
+def decode(in_pass):
+    out_pass = ""
+    while int(in_pass) > 0:
+        num = int(in_pass)%10
+        if num-3>=0:
+            num-=3
+            out_pass = str(num) + out_pass
+        else:
+            num+=7
+            out_pass = str(num) + out_pass
+        in_pass = str(int(in_pass)//10)
+    return out_pass
+
 if __name__ == "__main__":
     remain=True
+    encoded_password = ""
     while remain == True:
         print("Menu")
         print("-------------")
@@ -11,10 +27,12 @@ if __name__ == "__main__":
         #My individual code
         if user_choice == "1":
             password=(input("Please enter your password to encode: "))
-            encoded_password=""
+            encoded_password = ""
             for num in password:
                 if num == "0" or num == "1"or num == "2"or num == "3"or num == "4"or num == "5"or num == "6":
-                    encoded_password+=str(int(num)+3)
+                    num = int(num)
+                    num+=3
+                    encoded_password+=str(num)
                 elif num == "7":
                     encoded_password+="0"
 
@@ -24,15 +42,13 @@ if __name__ == "__main__":
                 elif num == "9":
                     encoded_password+="2"
 
-                else:
-                    pass
             print("Your password has been encoded and stored!")
             print("")
 
 
-        #Partner code
+        #Alejandro Velez's code
         elif user_choice == "2":
-            pass
+            print("The encoded password is " + encoded_password+", and the original password is " + decode(encoded_password) +".\n")
 
 
 
